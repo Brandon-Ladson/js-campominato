a6d61469405bb4aa25b0b2180d1e0016ad91593a
Brandon Ladson <brandonladson1999@gmail.com>
2020-10-28 18:54:49 +0100

    primo tentativo di creazione di un ciclo per far inserire all'utente i suoi numeri

--- js/script.js
+++ js/script.js
@@ -8,26 +8,29 @@
 
 
 // ciclo per 16 numeri da 1 a 100 generati dal pc
+var listaNumeriPc = [];
+
 for (var i = 0; i < 16; i++) {
   var numeriPc = genaratoreNumeriRandom(100);
+  listaNumeriPc.push(numeriPc);
   console.log("i numeri del pc sono: " + numeriPc);
 }
 
-
-// for (var i = 0; i < numeriPc.length; i++) {
-//   var ele = numeriPc[i];
-// }
-//
-// var arrayProva = [];
-// arrayProva.push(ele);
-// console.log(arrayProva);
-
-
 // chiedo all'utente un numero, sempre diverso, compreso tra 1 e 100
-// var numeriUtente = "";
-for (var i = 0; i != numeriPc; i++) {
+var numeriUguali = false;
+for (var i = 0; i < listaNumeriPc.length; i++) {
   var numeriUtente = parseInt(prompt("inserisci un numero tra 1 e 100"));
   console.log("i numeri dell'utente sono: " + numeriUtente);
+
+  if (numeriUtente === listaNumeriPc[i]) {
+    numeriUguali = true;
+  }
+}
+
+if (numeriUguali === true) {
+  console.log("HAI PERSO");
+} else {
+  console.log("HAI VINTO");
 }
 
 // se il numero inserito dall'utente è tra quelli generati dal pc finisce la partita
