ad0748c52ac1d01a65b1ca2f6a0b5ea961c27bbe
Brandon Ladson <brandonladson1999@gmail.com>
2020-10-29 16:23:34 +0100

    rifinitura regole js e creazione stile css

--- js/script.js
+++ js/script.js
@@ -15,42 +15,56 @@ for (var i = 0; i < 16; i++) {
 
   if (listaNumeriPc.indexOf(numeriPc) === -1) {
     listaNumeriPc.push(numeriPc);
+  } else {
+    i--;
   }
-
 }
 
-console.log(listaNumeriPc);
+// console.log(listaNumeriPc);
 
 // chiedo all'utente un numero, sempre diverso, compreso tra 1 e 100
 var listaNumeriUtente = [];
 var puntiUtente = 0;
+var gameOver = false;
 
-for (var i = 0; i < 84; i++) {
-  var numeriUtente = parseInt(prompt("inserisci un numero tra 1 e 100"));
+do {
+  for (var i = 0; i < 84; i++) {
 
-  // se il numero inserito dall'utente non è ancora stao inserito allora aggiungilo alla lista
-  if (listaNumeriUtente.indexOf(numeriUtente) === -1) {
-    listaNumeriUtente.push(numeriUtente);
+    var numeriUtente = parseInt(prompt("inserisci un numero tra 1 e 100"));
 
-  // altrimente non aggiungerlo e compare un messaggo di avviso
-  } else {
-    console.log("ha gia inserito questo numero");
+    // se il numero inserito dall'utente non è ancora stao inserito allora aggiungilo alla lista
+    if (listaNumeriUtente.indexOf(numeriUtente) === -1) {
+      listaNumeriUtente.push(numeriUtente);
+      gameOver = true;
+      puntiUtente++;
+
+    // altrimente non aggiungerlo e compare un messaggo di avviso
+    } else {
+      // console.log("ha gia inserito questo numero");
+      alert("ha gia inserito questo numero");
+    }
+
+    // console.log(listaNumeriUtente);
+
+    // se il numero inserito dall'utente è presete tra quelli del pc termina il gioco
+    if (listaNumeriPc.indexOf(numeriUtente) !== -1) {
+      // console.log("sei esploso");
+      puntiUtente--;
+      i = 84;
+      alert("BOOM!!!");
+    }
+
+    // console.log(puntiUtente);
   }
 
-  console.log(listaNumeriUtente);
+} while (gameOver != true);
 
+// stampa in pagina il punteggio dell'utente, i suoi numeri e quelli del pc
+document.getElementById('punti').innerHTML = "il tuo punteggio è: " + puntiUtente;;
 
-  // se il numero inserito dall'utente è tra quelli generati dal pc finisce la partita
-  if (listaNumeriPc.indexOf(numeriUtente) !== -1) {
-    console.log("sei esploso");
+document.getElementById('numeriutente').innerHTML = "i numeri da te inseriti sono: " + listaNumeriUtente;
 
-  // altrimenti incrementi il punteggio dell'utente
-  } else {
-    puntiUtente++;
-  }
-
-  console.log(puntiUtente);
-}
+document.getElementById('numeripc').innerHTML = "i numeri del pc sono: " + listaNumeriPc;
 
 
 // funzioni
